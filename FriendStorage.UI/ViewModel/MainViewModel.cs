edce8f2ce70b47215b066a3fd77fa9618c3c05b1
Lawrence LCI <lcheunglci@gmail.com>
2022-04-19 23:34:23 -0700

    Add the 2 properties to the MainViewModel for selection support in the MainView

--- FriendStorage.UI/ViewModel/MainViewModel.cs
+++ FriendStorage.UI/ViewModel/MainViewModel.cs
@@ -1,15 +1,29 @@
-﻿namespace FriendStorage.UI.ViewModel
+﻿using Prism.Events;
+using System;
+using System.Collections.ObjectModel;
+
+namespace FriendStorage.UI.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        private FriendEditViewModel _selectedFriendViewModel;
+        private Func<IFriendEditViewModel> _friendEditVmCreator;
 
-        public MainViewModel(INavigationViewModel navigationViewModel)
+
+        public MainViewModel(INavigationViewModel navigationViewModel, Func<IFriendEditViewModel> friendEditVmCreator, IEventAggregator eventAggregator)
         {
             NavigationViewModel = navigationViewModel;
-
+            FriendEditViewModels = new ObservableCollection<IFriendEditViewModel>();
+            _friendEditVmCreator = friendEditVmCreator;
         }
 
         public INavigationViewModel NavigationViewModel { get; private set; }
+
+        public ObservableCollection<IFriendEditViewModel> FriendEditViewModels { get; private set; }
+
+
+        public IFriendEditViewModel SelectedFriendViewModel { get => _selectedFriendViewModel; set => _selectedFriendViewModel = value; }
+
 
         public void Load()
         {
