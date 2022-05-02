7c090839d85c10b5086f4b51af4bd2f059c5ae3c
Lawrence LCI <lcheunglci@gmail.com>
2022-05-02 00:05:09 -0700

    Add messagedialogservice

---
+++ FriendStorage.UI/Dialogs/MessageDialogService.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace FriendStorage.UI.Dialogs
+{
+    public class MessageDialogService : IMessageDialogService
+    {
+        public MessageDialogResult ShowYesNoDialog(string title, string message)
+        {
+            return MessageBox.Show(message, title, MessageBoxButton.YesNo) == MessageBoxResult.Yes ? MessageDialogResult.Yes : MessageDialogResult.No;
+        }
+    }
+}
